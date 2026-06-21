64dfab139fb146914f112f10be3aff1df5c10b7e
Tsinjo99 <tsinjomaintso@gmail.com>
2026-06-21 15:07:07 +0300

    [Sprint-2] Ajout un methode avec annotation Mapping

--- Framework/src/main/java/controllers/TestContoller.java
+++ Framework/src/main/java/controllers/TestContoller.java
@@ -1,7 +1,17 @@
 package controllers;
 
 import ControllerPerso.Controller;
+import ControllerPerso.Mapping;
 
 @Controller
 public class TestContoller {
+    @Mapping("accueil")
+    public void afficherAccueil() {
+        System.out.println("Méthode accueil appelée !");
+    }
+
+    @Mapping("profil")
+    public void afficherProfil() {
+        System.out.println("Méthode profil appelée !");
+    }
 }
